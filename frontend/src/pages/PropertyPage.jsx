7be8d1d142648171a4e9a1b61f87427696996f55
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-10 11:24:31 +0200

    Step 2: Reading and Deleting a Single property

---
+++ frontend/src/pages/PropertyPage.jsx
@@ -0,0 +1,86 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {toast} from 'react-toastify';
+
+const PropertyPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const deleteProperty = async (id) => {
+    try {
+      const res = await fetch(`/api/properties/${id}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to delete property");
+      }
+    } catch (error) {
+      console.error("Error deleting property!", error);
+      toast.error("Error deleting property!");
+    }
+  };
+
+  useEffect(() => {
+    const fetchProperty = async () => {
+      try {
+        console.log("id: ", id);
+        const res = await fetch(`/api/properties/${id}`);
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await res.json();
+        setProperty(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperty();
+  }, [id]);
+
+  const onDeleteClick = (propertyId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?" + propertyId
+    );
+    if (!confirm) return;
+
+    deleteProperty(propertyId);
+    toast.success("Property Deleted Successfully!");
+    navigate("/");
+  };
+
+  return (
+    <div className="property-preview">
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <h2>{property.title}</h2>
+          <p>Type: {property.type}</p>
+          <p>Description: {property.description}</p>
+          <p>Price: {property.price}</p>
+          <p>Location Address: {property.location.address}</p>
+          <p>Location City: {property.location.city}</p>
+          <p>Location State: {property.location.state}</p>
+          <p>Location Zip Code: {property.location.zipCode}</p>
+          <p>Square Feet: {property.squareFeet}</p>
+          <p>Year Built: {property.yearBuilt}</p>
+          <div className="align-row">
+          <Link to={`/edit-property/${id}`} className="btn"> Edit </Link>
+          <Link to="/" onClick={() => onDeleteClick(property._id)} className="btn">Delete</Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PropertyPage;
