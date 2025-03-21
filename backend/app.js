f7e273c9f80cf25d5c22d09845e9ac1ca223bd25
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-21 12:44:46 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
-const jobRouter = require("./routes/propertyRouter");
+const propertyRouter = require("./routes/propertyRouter");
 const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -13,8 +13,8 @@ app.use(express.json());
 // Connect to the database
 connectDB();
 
-// Use the jobRouter for all "/jobs" routes
-app.use("/api/properties", jobRouter);
+// Use the propertyRouter for all "/property" routes
+app.use("/api/properties", propertyRouter);
 
 // Middlewares
 app.use(unknownEndpoint);
